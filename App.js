49291808a3388042d4ea506417dc6465496c5113
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-14 03:53:54 +0900

    get location

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class extends React.Component{
+  state = {
+    isLoading: true,
+  };
+
+  getLocation = async() => {
+    try{
+      const response = await Location.requestPermissionsAsync();
+      const {coords: {latitude,longitude}} = await Location.getCurrentPositionAsync();
+      //console.log(response);
+      //console.log(latitude,longitude);
+      this.setState({isLoading: false});
+    }catch(error){
+      Alert.alert("can't find you", "so sad");
+    }
+  }
+
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render() {
+    const {isLoading} = this.state;
+    return (
+      isLoading ? <Loading /> : null
+    );
+  }
+  
 }
 
