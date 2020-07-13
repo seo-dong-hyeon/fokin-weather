587e9e9f46c9e0f9aa881d77390cf7f10b6f58c5
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-14 03:17:30 +0900

    loading screen

--- App.js
+++ App.js
@@ -1,35 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-      </View>
-      <View style={styles.blueView}>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <Loading />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, // 모든 공간을 사용
-    backgroundColor: 'white',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    width: "100%",
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor: "blue",
-  },
-});
